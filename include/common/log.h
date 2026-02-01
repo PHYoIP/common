dfab72ed44f23936df49c2e2f7873a2aed1cf9c9
Oliver Blaser <58989552+oblaser@users.noreply.github.com>
2026-02-01 18:46:53 +0100

    added log (stderr)

---
+++ include/common/log.h
@@ -0,0 +1,180 @@
+/*
+author          Oliver Blaser
+date            01.02.2026
+copyright       GPL-3.0 - Copyright (c) 2026 Oliver Blaser
+*/
+
+/*
+    This log is written to stderr, for user interaction see cli.h
+*/
+
+#ifndef IG_COMMON_LOG_H
+#define IG_COMMON_LOG_H
+
+#include <stdio.h>
+
+#include "common/ansi-esc.h"
+
+
+
+#define LOG_LEVEL_OFF (0)
+#define LOG_LEVEL_ERR (1)
+#define LOG_LEVEL_WRN (2)
+#define LOG_LEVEL_INF (3)
+#define LOG_LEVEL_DBG (4)
+
+
+
+// stringify
+#define ___LOG_STR_HELPER(x) #x
+#define ___LOG_STR(x)        ___LOG_STR_HELPER(x)
+
+// optional args
+#define ___LOG_OPT_VA_ARGS(...) , ##__VA_ARGS__
+
+
+
+#ifndef CONFIG_LOG_MAX_LEVEL
+#define CONFIG_LOG_MAX_LEVEL LOG_LEVEL_INF
+#endif
+
+#ifndef COMMON_LOG_DEFINE_FUNCTIONS
+#ifndef LOG_MODULE_LEVEL
+#error "define LOG_MODULE_LEVEL before including log.h"
+#endif
+#ifndef LOG_MODULE_NAME
+#error "define LOG_MODULE_NAME before including log.h"
+#endif
+#endif // COMMON_LOG_DEFINE_FUNCTIONS
+
+#ifndef LOG_TIMESTAMP_STR
+
+#ifdef __cplusplus
+#define LOG_TIMESTAMP_STR LOG_tNow_local_iso8601().c_str()
+#else
+#define LOG_TIMESTAMP_STR LOG_tNow_local_iso8601()
+#endif
+
+#endif // LOG_TIMESTAMP_STR
+
+
+
+#define LOG_COL_DEFAULT SGR_FG_DEFAULT
+#define LOG_COL_ERR     SGR_FG_BRED
+#define LOG_COL_WRN     SGR_FG_BYELLOW
+#define LOG_COL_INF     LOG_COL_DEFAULT
+#define LOG_COL_DBG     SGR_FG_BBLACK
+
+
+// clang-format off
+#define LOG_ERR(msg, ...) fprintf(stderr, "%s" "[%s] " "%s" ___LOG_STR(LOG_MODULE_NAME) " <ERR> " msg "%s" "\n", ansi::esc[CSI_EL], LOG_TIMESTAMP_STR, ansi::esc[LOG_COL_ERR] ___LOG_OPT_VA_ARGS(__VA_ARGS__), ansi::esc[SGR_RESET])
+#define LOG_WRN(msg, ...) fprintf(stderr, "%s" "[%s] " "%s" ___LOG_STR(LOG_MODULE_NAME) " <WRN> " msg "%s" "\n", ansi::esc[CSI_EL], LOG_TIMESTAMP_STR, ansi::esc[LOG_COL_WRN] ___LOG_OPT_VA_ARGS(__VA_ARGS__), ansi::esc[SGR_RESET])
+#define LOG_INF(msg, ...) fprintf(stderr, "%s" "[%s] " "%s" ___LOG_STR(LOG_MODULE_NAME) " <INF> " msg "%s" "\n", ansi::esc[CSI_EL], LOG_TIMESTAMP_STR, ansi::esc[LOG_COL_INF] ___LOG_OPT_VA_ARGS(__VA_ARGS__), ansi::esc[SGR_RESET])
+#define LOG_DBG(msg, ...) fprintf(stderr, "%s" "[%s] " "%s" ___LOG_STR(LOG_MODULE_NAME) " <DBG> " "%s" "%s():%i" "%s" " " msg "%s" "\n", ansi::esc[CSI_EL], LOG_TIMESTAMP_STR, ansi::esc[LOG_COL_DEFAULT], ansi::esc[LOG_COL_DBG], __func__, (int)(__LINE__), ansi::esc[LOG_COL_DEFAULT] ___LOG_OPT_VA_ARGS(__VA_ARGS__), ansi::esc[SGR_RESET])
+// clang-format on
+
+
+
+#define LOG_LEVEL_IS_ENABLED(_level) ((LOG_MODULE_LEVEL >= (_level)) && (CONFIG_LOG_MAX_LEVEL >= (_level)))
+
+
+
+#if !LOG_LEVEL_IS_ENABLED(LOG_LEVEL_DBG)
+#undef LOG_DBG
+#define LOG_DBG(...) (void)0
+#endif
+
+#if !LOG_LEVEL_IS_ENABLED(LOG_LEVEL_INF)
+#undef LOG_INF
+#define LOG_INF(...) (void)0
+#endif
+
+#if !LOG_LEVEL_IS_ENABLED(LOG_LEVEL_WRN)
+#undef LOG_WRN
+#define LOG_WRN(...) (void)0
+#endif
+
+#if !LOG_LEVEL_IS_ENABLED(LOG_LEVEL_ERR)
+#undef LOG_ERR
+#define LOG_ERR(...) (void)0
+#endif
+
+
+
+#ifdef __cplusplus
+#include <string>
+std::string LOG_tNow_local_iso8601();
+#else  // __cplusplus
+const char* LOG_tNow_local_iso8601();
+#endif // __cplusplus
+
+
+
+#endif // IG_COMMON_LOG_H
+
+
+
+// #####################################################################################################################
+// #####################################################################################################################
+// #####################################################################################################################
+
+
+
+#ifdef COMMON_LOG_DEFINE_FUNCTIONS
+#undef COMMON_LOG_DEFINE_FUNCTIONS
+
+#include <inttypes.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <time.h>
+
+#ifdef __cplusplus
+#include <string>
+#endif
+
+
+static inline void ___LOG_t_local_iso8601(char* buffer, size_t size, time_t t)
+{
+#ifdef _MSC_VER
+#pragma warning(suppress :4996)
+    const struct tm* tm = localtime(&t);
+#else
+    const struct tm* tm = localtime(&t);
+#endif
+
+    if (!(tm && (strftime(buffer, size, "%FT%T", tm) > 0)))
+    {
+#ifdef _MSC_VER
+#pragma warning(suppress :4996)
+        sprintf(buffer, "%" PRIi64, (int64_t)t);
+#else
+        sprintf(buffer, "%" PRIi64, (int64_t)t);
+#endif
+    }
+}
+
+
+#ifdef __cplusplus
+
+std::string LOG_tNow_local_iso8601()
+{
+    char buffer[25];
+    ___LOG_t_local_iso8601(buffer, sizeof(buffer), time(NULL));
+    return buffer;
+}
+
+#else // __cplusplus
+
+const char* LOG_tNow_local_iso8601()
+{
+#warning "LOG_tNow_local_iso8601() is not thread safe"
+    static char buffer[25];
+    ___LOG_t_local_iso8601(buffer, sizeof(buffer), time(NULL));
+    return buffer;
+}
+
+#endif // __cplusplus
+
+
+#endif // COMMON_LOG_DEFINE_FUNCTIONS
